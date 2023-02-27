9d752b3968e3dbdb168bb4b94d87f054c0d68c47
7007singh <shw12021982@gmail.com>
2023-02-27 11:16:17 +0530

    Welcome to Indian States Census Analyser Program

---
+++ IndianStatesCensusAnalyserProgram/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStatesCensusAnalyserProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("___Welcome to Indian States Census Analyser Program___");
+        }
+    }
+}
