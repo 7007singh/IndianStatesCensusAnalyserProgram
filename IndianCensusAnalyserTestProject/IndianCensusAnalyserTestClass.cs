a69e62e0be44570f595ef648c2fe671c5617f318
7007singh <shw12021982@gmail.com>
2023-03-12 17:13:16 +0530

    [Shweta]Add: Given the State Census CSV File if incorrect Returns a custom Exception

--- IndianCensusAnalyserTestProject/IndianCensusAnalyserTestClass.cs
+++ IndianCensusAnalyserTestProject/IndianCensusAnalyserTestClass.cs
@@ -30,9 +30,9 @@ namespace IndianCensusAnalyserTestProject
 
         //[TestMethod]
         //[DataRow(incorrectHeader, "Incorrect header in Data")]
-        ////[DataRow(incorrectDelimiter, "File Containers Wrong Delimiter")]
-        ////[DataRow(incorrectFileType, "Invalid file type")]
-        ////[DataRow(incorrectFile, "File Not Found")]
+        //[DataRow(incorrectDelimiter, "File Containers Wrong Delimiter")]
+        //[DataRow(incorrectFileType, "Invalid file type")]
+        //[DataRow(incorrectFile, "File Not Found")]
         //public void Given_State_CensusCSVFile_If_Incorrect_Returns_FileNotFound_Exception(string file, string expected)
         //{
         //    var result = Assert.ThrowsException<CensusAnalyserException>(() => factory.LoadCsvData(CensusAnalyser.Country.INDIA, file, "State,Population,AreaInSqKm,DensityPerSqKm"));
@@ -40,12 +40,20 @@ namespace IndianCensusAnalyserTestProject
         //}
 
         string indianStateCode = @"C:\Users\Shweta\source\repos\IndianStatesCensusAnalyserProgram\IndianStatesCensusAnalyserProgram\CSVFile\IndianStateCode.csv";
+        public const string incorrectFile = @"C:\Users\Shweta\source\repos\IndianStatesCensusAnalyserProgram\IndianStatesCensusAnalyserProgram\CSVFile\IndianState.txt";
 
         [TestMethod]
         public void Given_CSVFile_Should_Return_NoOfRecord()
         {
             dict = factory.LoadCsvData(CensusAnalyser.Country.INDIA, indianStateCode, "SrNo,State Name,TIN,StateCode");
             Assert.AreEqual(37, dict.Count);
+        }
+        [TestMethod]
+        [DataRow(incorrectFile, "File Not Found")]
+        public void Given_State_CensusCSVFile_If_Incorrect_Returns_FileNotFound_Exception(string file, string expected)
+        {
+            var result = Assert.ThrowsException<CensusAnalyserException>(() => factory.LoadCsvData(CensusAnalyser.Country.INDIA, file, "SrNo,State Name,TIN,StateCode"));
+            Assert.AreEqual(expected, result.Message);
         }
     }
 }
