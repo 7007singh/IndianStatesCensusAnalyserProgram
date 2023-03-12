11aa475bdb7136bfc036d7641020719cd9d5c7af
7007singh <shw12021982@gmail.com>
2023-03-12 12:33:47 +0530

    [Shweta]Add: Given the State Census CSV File if incorrect Returns a custom Exception

--- IndianCensusAnalyserTestProject/IndianCensusAnalyserTestClass.cs
+++ IndianCensusAnalyserTestProject/IndianCensusAnalyserTestClass.cs
@@ -4,6 +4,7 @@ using IndianStateCensusAnalyser;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using IndiaCensusDataDemo;
 
 namespace IndianCensusAnalyserTestProject
 {
@@ -13,12 +14,20 @@ namespace IndianCensusAnalyserTestProject
         Dictionary<string, CensusDTO> dict = new Dictionary<string, CensusDTO>();
         CSVAdapterFactory factory = new CSVAdapterFactory();
         string indianPopulation = @"C:\Users\Shweta\source\repos\IndianStatesCensusAnalyserProgram\IndianStatesCensusAnalyserProgram\CSVFile\IndianStateCensusData.csv";
-
+        string incorrectFile = @"C:\Users\Shweta\source\repos\IndianStatesCensusAnalyserProgram\IndianStatesCensusAnalyserProgram\CSVFile\IndianCensus.txt";
+        
         [TestMethod]
         public void Given_CSVFile_Should_Return_NoOfRecord()
         {
             dict = factory.LoadCsvData(CensusAnalyser.Country.INDIA, indianPopulation, "State,Population,AreaInSqKm,DensityPerSqKm");
             Assert.AreEqual(29, dict.Count);
+        }
+        [TestMethod]
+        public void Given_State_CensusCSVFile_If_Incorrect_Returns_FileNotFound_Exception()
+        {
+            string expected = "File Not Found";
+            var result = Assert.ThrowsException<CensusAnalyserException>(() => factory.LoadCsvData(CensusAnalyser.Country.INDIA, expected, "State,Population,AreaInSqKm,DensityPerSqKm"));
+            Assert.AreEqual("File Not Found", result.Message);
         }
     }
 }
